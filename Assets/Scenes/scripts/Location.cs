d3ce7b39540eb4abb3386233d8396de9d502b7be
code-11 <brendan.ritter@yahoo.com>
2020-09-18 19:01:43 -0400

    Initial commit

---
+++ Assets/Scenes/scripts/Location.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using XNode;
+
+public class Location : Node {
+
+    public string locationName;
+
+    [TextArea(15, 20)]
+    public string description;
+
+    [Output] public string toFrontDoor;
+    [Output] public string toKitchen;
+    [Output] public string toBedroom;
+    [Output] public string toBathroom;
+
+    // Use this for initialization
+    protected override void Init() {
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return null; // Replace this
+	}
+}
